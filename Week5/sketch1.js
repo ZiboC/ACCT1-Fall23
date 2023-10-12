3a3915d40582d03116129c9c48ccc1564992f088
ZiboC <145066908+ZiboC@users.noreply.github.com>
2023-10-12 16:59:16 -0700

    Week4 Inclass
    Moving Circle

--- Week5/sketch1.js
+++ Week5/sketch1.js
@@ -1,15 +1,33 @@
+let angle = 0.67
+
 function setup(){
         let myCanvas = createCanvas(600, 600)
-        myCanvas.parent("sketchHolder")
-        background(200, 230, 160)
+        myCanvas.parent("sketchHolder1")
+    
+        setInterval(function(){
+            console.log("fire")
+            drawGrid(20)
+        }, 2000)
+    }
 
-        rotate(QUARTER_PI)
+function draw(){
+        background(200, 230, 160)
+        
+        push()
+        translate(100,  100)
+        rotate(angle)
         drawGrid(60)
         fill(0, 0, 0)
-        rect(100, 100, 100, 150)
+        rect(0, 0, 100, 150) 
+        pop()
+
+        circle(150, 150, 100)
+
+        angle+= 0.01;
     }
 
 function drawGrid(numSegments = 10){
+    
     let interval = width/numSegments
     for(let x = 0; x <= numSegments; x++){
         line(x*interval, 0, x*interval, height)   
@@ -19,3 +37,4 @@ function drawGrid(numSegments = 10){
         line(0, y*interval, width, y*interval)
     }
 }
+
